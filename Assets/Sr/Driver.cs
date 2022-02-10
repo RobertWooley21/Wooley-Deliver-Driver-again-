b349a4c742fc2109f883dae63f68fc841602ada7
RobertWooley <wooley278@morrisville.edu>
2022-02-10 12:38:05 -0500

    Input.GetAxcle"()"

--- Assets/Sr/Driver.cs
+++ Assets/Sr/Driver.cs
@@ -1,3 +1,4 @@
+using System;
 using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
@@ -6,7 +7,9 @@ public class Driver : MonoBehaviour
 {
     [SerializeField] float moveSpeed = 1f;
     [SerializeField] float Drivingcontrol = 0.1f;
-   
+    private Input streerSpeed;
+    private Time deltaTime;
+
     // Start is called before the first frame update
     void Start()
     {
@@ -16,7 +19,14 @@ public class Driver : MonoBehaviour
     // Update is called once per frame
     void Update()
     {
+        _ = Input./GetAxis("Horizontal") * streerSpeed * Time.deltaTime;
+        float moveAmount = Input. GetAxis("Vertical") * moveSpeed * Time. deltaTime;
         transform.Rotate(0, 0, Drivingcontrol);
         transform.Translate(0, 0.01f, 0);
+    }
+
+    private Input GetAxis(string v)
+    {
+        throw new NotImplementedException();
     }
 }
