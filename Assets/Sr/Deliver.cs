3b5629359608c1501d385c838309630922ac22bc
RobertWooley <wooley278@morrisville.edu>
2022-02-17 09:07:49 -0500

    How To Destory An Objcet

--- Assets/Sr/Deliver.cs
+++ Assets/Sr/Deliver.cs
@@ -1,5 +1,6 @@
 using System.Collections;
 using System.Collections.Generic;
+using System.Runtime.ConstrainedExecution;
 using UnityEngine;
 
 public class NewBehaviourScript : MonoBehaviour
@@ -8,26 +9,44 @@ public class NewBehaviourScript : MonoBehaviour
     private object debug;
 
     public bool The { get; private set; }
+    public object package { get; private set; }
 
     void OnCollisionEnter2D(Collision2D other, IReadOnlyCollection readOnlyCollection)           {
-        Debug.Log(readOnlyCollectionneedtoslowdown!);
+        GetDebug().Log(readOnlyCollectionneedtoslowdown!);
         
     {
     
     }
-     void OnCollisionEnter2D(Collision2D Cool) { }
-
-    // Update is called once per frame
-    void Update()
+     void OnCollisionEnter2D(Collision2D other)
+        {
+            Debug.Log("I have Collided with something")
+        }
+       
     {
-            if (other.tag != 'package');
-            {
-            }
-            else
-                debug.log;
-                if (The thing we trigger );
+           void OnTriggerEnter2D(Collider2D other)
+            { SerializeField float(0.5fS);
+                bool haspackage;false;
+                bool haspackage;= true;
+
+                    if tag(other.tag== package"costmer") && haspackage
+                
+                if (other.tag == "package" && !b1HasPackage)
+                    Debug.Log("package pickage up.");
+                b1HasPackage = true;
+                SpriteRenderer.color = c1HasPackage
+                Destroy(other.gameObject && !nothas package )                
+                    object log = debug.log;
+            if (The { } thing we; 
+
+            object trigger = null;
+            trigger;
     }
 }
+
+    private object GetDebug()
+    {
+        return debug;
+    }
 
     private interface IReadOnlyCollection
     {
--- Assets/Sr/Fllowing Camera.cs
+++ Assets/Sr/Fllowing Camera.cs
@@ -10,7 +10,7 @@ public class FllowingCamra : MonoBehaviour
     // Start is called before the first frame update
     void Start()
     {
-        [SerializeField] gameObject thingtoFollow;
+        [SerializeField] GameObject thingtoFollow;
         
     }
 
